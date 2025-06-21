4cf4c58f4f7185d43018f4dd5a4868c46756ac8a
Jefferson <jsv@hotmail.com.br>
2025-06-21 11:07:23 -0300

    findById implementation

--- src/model/entities/DaoFactory.java
+++ src/model/entities/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.entities;
 
+import db.DB;
 import model.dao.SellerDao;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao creatSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
