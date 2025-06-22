5a4d615b58bbbe7f271fea50a3b67d9625caee1b
Jefferson <jsv@hotmail.com.br>
2025-06-22 11:59:32 -0300

    findByDepartment and implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args){
@@ -13,8 +14,14 @@ public class Program {
         System.out.println("===== Test 1: seller findById =====");
         SellerDao sellerDao = DaoFactory.creatSellerDao();//dessa forma o programa não conhece as dependencias só a interface
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("===== Test 2: seller findByDepartment =====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
 
     }
 }
