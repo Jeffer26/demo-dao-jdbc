86cc18be6f219701d96b48d8d3df91fdcdd04bff
Jefferson <jsv@hotmail.com.br>
2025-06-22 12:15:29 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,6 +15,7 @@ public class Program {
         SellerDao sellerDao = DaoFactory.creatSellerDao();//dessa forma o programa não conhece as dependencias só a interface
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println();
 
         System.out.println("===== Test 2: seller findByDepartment =====");
         Department department = new Department(2, null);
@@ -22,6 +23,16 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+        System.out.println();
+
+
+        System.out.println("===== Test 3: seller findByAll =====");
+        list = sellerDao.findAll();
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
+        System.out.println();
+
 
     }
 }
