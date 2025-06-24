044ebf8d968832e1c9fc1e302b9c6434c9fe0536
Jefferson <jsv@hotmail.com.br>
2025-06-24 12:52:45 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,5 +40,11 @@ public class Program {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
 
+        System.out.println("===== Test 5: seller Update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
